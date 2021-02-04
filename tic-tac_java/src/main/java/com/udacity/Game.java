444870d139e77f6b7c37db3279de958847765dec
madasilva1 <madasilva195@gmail.com>
2021-02-04 10:59:57 -0500

    main class made array stile

--- tic-tac_java/src/main/java/com/udacity/Game.java
+++ tic-tac_java/src/main/java/com/udacity/Game.java
@@ -209,7 +209,7 @@ public class Game {
      * Main function
      * @param args command line arguments
      */
-    public static void main(String args[]){
+    public static void main(String[] args){
         Game game = new Game();
         gui = new GameUI(game);
     }
